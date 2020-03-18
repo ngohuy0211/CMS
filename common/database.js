1fd7058d626e64cbb8a19e1812e0d4432ca373d4
Phan Ngoc  Hoang <hoangpngch17194@fpt.edu.vn>
2020-03-18 20:37:24 +0700

    Update database.js

--- common/database.js
+++ common/database.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose')
-module.exports = ()=>{
-    mongoose.connect(
-        'mongodb://localhost:27017/Cms_database',
-        {useNewUrlParser: true}
-    )
+const mongoose = require("mongoose");
+module.exports = () => {
+  mongoose.set("useCreateIndex", true);
+  mongoose.connect("mongodb://localhost:27017/Cms_database", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
 
-    return mongoose
-}
+  return mongoose;
+};
