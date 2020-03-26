53127052ddc82b0f3c1cc64aaa43db0c3c7e9b4b
Phan Ngoc  Hoang <hoangpngch17194@fpt.edu.vn>
2020-03-26 11:57:48 +0700

    connect application

--- common/database.js
+++ common/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 module.exports = () => {
   mongoose.set("useCreateIndex", true);
-  mongoose.connect("mongodb://localhost:27017/Cms_database", {
+  mongoose.connect("mongodb://localhost:27017/Cms_database" || process.env.DATABASE_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true
   });
